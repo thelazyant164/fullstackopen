72fc892eda4b579cc4e0b750af2ffe8d8bb191d0
Lý Thiên Ân <lythienan164@gmail.com>
2021-11-09 11:29:01 +0700

    [R] - added smaller components "Header", "Content" and "Total

--- part1/1.1/src/App.js
+++ part1/1.1/src/App.js
@@ -1,5 +1,25 @@
 import React from 'react'
 
+const Header = props => (
+  <>
+    <p>This course is named "{props.course}".</p>
+  </>
+)
+
+const Content = props => (
+  <>
+    <p>{props.part1}: {props.exercises1} exercises</p>
+    <p>{props.part2}: {props.exercises2} exercises</p>
+    <p>{props.part3}: {props.exercises3} exercises</p>
+  </>
+)
+
+const Total = props => (
+  <>
+    <p>Total number of exercises: {props.exercises1 + props.exercises2 + props.exercises3} exercises.</p>
+  </>
+)
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -10,19 +30,11 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
+    <>
+      <Header course = {course}></Header>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}></Content>
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}></Total>
+    </>
   )
 }
 
