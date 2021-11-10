08135565e2773559e3f3fd4350d5369481c83577
Lý Thiên Ân <lythienan164@gmail.com>
2021-11-10 09:39:59 +0700

    [R] - put all parts into array

--- part1/1.1/src/App.js
+++ part1/1.1/src/App.js
@@ -28,24 +28,26 @@ const Total = props => (
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+  ]
 
   return (
     <>
       <Header course = {course}></Header>
-      <Content part1 = {part1.name} part2 = {part2.name} part3 = {part3.name} exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}></Content>
-      <Total exercises1 = {part1.exercises} exercises2 = {part2.exercises} exercises3 = {part3.exercises}></Total>
+      <Content part1 = {parts[0].name} part2 = {parts[1].name} part3 = {parts[2].name} exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises}></Content>
+      <Total exercises1 = {parts[0].exercises} exercises2 = {parts[1].exercises} exercises3 = {parts[2].exercises}></Total>
     </>
   )
 }
